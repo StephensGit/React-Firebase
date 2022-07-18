2921b62feb2f0b1c3aa63ca2c7bae8d957eaa5aa
Stephen Moran <stephenmoran@live.ie>
2022-07-18 20:42:33 +0100

    Logout hook functionality added

--- my-money/src/components/Navbar.js
+++ my-money/src/components/Navbar.js
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 // styles
 import styles from "./Navbar.module.css";
 
-import React from "react";
-
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
+
         <li>
-          <Link to="./Login">Login</Link>
+          <Link to="/login">Login</Link>
         </li>
         <li>
-          <Link to="./Signup">Signup</Link>
+          <Link to="/signup">Signup</Link>
+        </li>
+
+        <li>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
