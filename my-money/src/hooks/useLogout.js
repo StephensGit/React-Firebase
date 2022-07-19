538a19e6c2cdce95fea8f874033772857f83e140
Stephen Moran <stephenmoran@live.ie>
2022-07-19 07:07:08 +0100

    cleanup-functions

--- my-money/src/hooks/useLogout.js
+++ my-money/src/hooks/useLogout.js
@@ -1,32 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
   const logout = async () => {
+    console.log("logout");
     setError(null);
     setIsPending(true);
 
     try {
       // sign the user out
+      console.log("try");
       await projectAuth.signOut();
 
       // dispatch logout action
       dispatch({ type: "LOGOUT" });
 
       // update state
-      setIsPending(false);
-      setError(null);
+      if (!isCancelled) {
+        console.log("try/if");
+        setIsPending(false);
+        setError(null);
+      }
     } catch (err) {
-      console.log(err.message);
-      setError(err.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      }
     }
   };
+
+  useEffect(() => {
+    console.log("useEffect");
+    return setIsCancelled(true);
+  }, []);
 
   return { logout, error, isPending };
 };
